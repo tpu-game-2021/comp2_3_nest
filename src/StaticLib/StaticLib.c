ae2316ea5d0fc28d997ff4c31f710b98332c7203
SK2127043 <90638518+SK2127043@users.noreply.github.com>
2021-10-02 03:25:18 +0900

    Update StaticLib.c

--- src/StaticLib/StaticLib.c
+++ src/StaticLib/StaticLib.c
@@ -30,11 +30,52 @@ int calc_combinations_3(int sum, int val1, int val2, int val3)
 // val1 から val4 の任意の組み合わせで値の和がsumになる組み合わせは何個ありますか？
 int calc_combinations_4(int sum, int val1, int val2, int val3, int val4)
 {
-	return 0;
+	int num = 0;
+
+	if (sum == val1) num++;
+	if (sum == val2) num++;
+	if (sum == val3) num++;
+	if (sum == val4) num++;
+	if (sum == val1 + val2) num++;
+	if (sum == val1 + val3) num++;
+	if (sum == val1 + val4) num++;
+	if (sum == val2 + val3) num++;
+	if (sum == val2 + val4) num++;
+	if (sum == val3 + val4) num++;
+	if (sum == val1 + val2 + val3) num++;
+	if (sum == val1 + val2 + val4) num++;
+	if (sum == val1 + val3 + val4) num++;
+	if (sum == val2 + val3 + val4) num++;
+	if (sum == val1 + val2 + val3 + val4) num++;
+
+	return num;
 }
 
 // val_array の配列に格納されたarray_sizeの数字の任意の組み合わせで値の和がsumになる組み合わせは何個ありますか？
 int calc_combinations(int sum, const int* val_array, int array_size)
 {
-	return 0;
+	int num = 0;
+	int i;
+	int a;
+	int b = 1;
+	for (int i = 0; i < array_size; i++)
+	{
+		if (sum == val_array[i]) num++;
+		else
+		{
+			int SUM = 0;
+			for (int a = i; SUM < sum; a++)
+			{
+				SUM += val_array[a];
+			}
+			if (sum == SUM) num++;
+
+			if (i + 1 < sum)
+			{
+				b = b * (array_size - i) / (sum - i);
+			}
+		}
+	}
+	num *= b;
+	return num;
 }
