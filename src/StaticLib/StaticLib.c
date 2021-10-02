7b2580cb5cb822d1ea047537b09cf23348e18801
megutan30 <megameguroko@gmail.com>
2021-10-02 23:33:21 +0900

    関数の中身を実装しました。

--- src/StaticLib/StaticLib.c
+++ src/StaticLib/StaticLib.c
@@ -30,11 +30,62 @@ int calc_combinations_3(int sum, int val1, int val2, int val3)
 // val1 から val4 の任意の組み合わせで値の和がsumになる組み合わせは何個ありますか？
 int calc_combinations_4(int sum, int val1, int val2, int val3, int val4)
 {
-	return 0;
+
+	int num = 0;
+	if (sum == val1) num++;
+	if (sum == val2) num++;
+	if (sum == val3) num++;
+	if (sum == val4) num++;
+	if (sum == val1 + val2) num++;
+	if (sum == val1 + val3) num++;
+	if (sum == val1 + val4) num++;
+	if (sum == val2 + val3) num++;
+	if (sum == val2 + val4) num++;
+	if (sum == val3 + val4) num++;
+	if (sum == val1 + val2 + val3) num++;
+	if (sum == val1 + val2 + val4) num++;
+	if (sum == val1 + val3 + val4) num++;
+	if (sum == val2 + val3 + val4) num++;
+	if (sum == val1 + val2 + val3 + val4) num++;
+
+	return num;
 }
 
 // val_array の配列に格納されたarray_sizeの数字の任意の組み合わせで値の和がsumになる組み合わせは何個ありますか？
 int calc_combinations(int sum, const int* val_array, int array_size)
 {
-	return 0;
+	int i, j;
+	int total = 0;
+	int  num = 0;
+	float  num1 = 1;
+	float  num2 = 1;
+	//array_sizeCi+1
+
+	for (i = 0; i < array_size; i++)
+	{
+		total += val_array[i];
+		if (sum == total)
+		{
+			for (j = 0; j < i + 1; j++)
+			{
+				num1 *= ((float)(i+1) - j);
+			}
+			for (j = 0; j < i + 1; j++)
+			{
+				num2 *= ((float)array_size - j);
+			}
+			num = (int)(num2 / num1);
+			break;
+		}
+	}
+	
+	return num;
+
+
+	//if (array_size == sum)
+	//	return(1);
+	//else if (sum == 1)
+	//	return(array_size);
+	//else
+	//	return(calc_combinations(array_size - 1,  sum - 1) + calc_combinations(array_size - 1,  sum));
 }
