a5ed4f1ae565d80e29e7a4756b619ba448d4ea63
nisidayuki <90868334+nisidayuki@users.noreply.github.com>
2021-10-02 18:09:16 +0900

    途中までできました

--- src/StaticLib/StaticLib.c
+++ src/StaticLib/StaticLib.c
@@ -1,4 +1,4 @@
-#define WIN32_LEAN_AND_MEAN             // Windows ヘッダーからほとんど使用されていない部分を除外する
+#define WIN32_LEAN_AND_MEAN             // Windows ヘッダーからほとんど使用されていない部分を除外する#define WIN32_LEAN_AND_MEAN             // Windows ヘッダーからほとんど使用されていない部分を除外する
 #include "Windows.h"                    // Windows API の機能定義
 
 #include <stdbool.h>
@@ -30,11 +30,32 @@ int calc_combinations_3(int sum, int val1, int val2, int val3)
 // val1 から val4 の任意の組み合わせで値の和がsumになる組み合わせは何個ありますか？
 int calc_combinations_4(int sum, int val1, int val2, int val3, int val4)
 {
-	return 0;
+	int s = 0;
+
+	if (sum == val1) s++;
+	if (sum == val2) s++;
+	if (sum == val3) s++;
+	if (sum == val4) s++;
+	if (sum == val1 + val2) s++;
+	if (sum == val1 + val3) s++;
+	if (sum == val1 + val4) s++;
+	if (sum == val2 + val3) s++;
+	if (sum == val2 + val4) s++;
+	if (sum == val3 + val4) s++;
+	if (sum == val1 + val2 + val3) s++;
+	if (sum == val1 + val2 + val4) s++;
+	if (sum == val1 + val3 + val4) s++;
+	if (sum == val2 + val3 + val4) s++;
+	if (sum == val1 + val2 + val3 + val4) s++;
+
+	return s;
 }
 
 // val_array の配列に格納されたarray_sizeの数字の任意の組み合わせで値の和がsumになる組み合わせは何個ありますか？
 int calc_combinations(int sum, const int* val_array, int array_size)
 {
-	return 0;
+	int v = 0;
+	if (sum == sum) v++;
+	return v;
+
 }
