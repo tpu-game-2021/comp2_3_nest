1a01ea931526da19925e36770a1ebcaf53bb3727
matsuurataketo <g2127063@st.t-kougei.ac.jp>
2021-10-03 23:41:35 +0900

    関数の中身を追加しました。

--- src/StaticLib/StaticLib.c
+++ src/StaticLib/StaticLib.c
@@ -30,11 +30,132 @@ int calc_combinations_3(int sum, int val1, int val2, int val3)
 // val1 から val4 の任意の組み合わせで値の和がsumになる組み合わせは何個ありますか？
 int calc_combinations_4(int sum, int val1, int val2, int val3, int val4)
 {
-	return 0;
+	int pattern[11];
+	int count = 0;
+
+	pattern[0] = val1 + val2;
+	pattern[1] = val1 + val3;
+	pattern[2] = val1 + val4;
+	pattern[3] = val2 + val3;
+	pattern[4] = val2 + val4;
+	pattern[5] = val3 + val4;
+	pattern[6] = val1 + val2 + val3;
+	pattern[7] = val1 + val2 + val4;
+	pattern[8] = val1 + val3 + val4;
+	pattern[9] = val2 + val3 + val4;
+	pattern[10] = val1 + val2 + val3 + val4;
+
+	if (pattern[0] == sum)
+	{
+		count = count + 1;
+	}
+	if (pattern[1] == sum)
+	{
+		count = count + 1;
+	}
+	if (pattern[2] == sum)
+	{
+		count = count + 1;
+	}
+	if (pattern[3] == sum)
+	{
+		count = count + 1;
+	}
+	if (pattern[4] == sum)
+	{
+		count = count + 1;
+	}
+	if (pattern[5] == sum)
+	{
+		count = count + 1;
+	}
+	if (pattern[6] == sum)
+	{
+		count = count + 1;
+	}
+	if (pattern[7] == sum)
+	{
+		count = count + 1;
+	}
+	if (pattern[8] == sum)
+	{
+		count = count + 1;
+	}
+	if (pattern[9] == sum)
+	{
+		count = count + 1;
+	}
+	if (pattern[10] == sum)
+	{
+		count = count + 1;
+	}
+
+
+	if (val1 == sum)
+	{
+		count = count + 1;
+	}
+	if (val2 == sum)
+	{
+		count = count + 1;
+	}
+	if (val3 == sum)
+	{
+		count = count + 1;
+	}
+	if (val4 == sum)
+	{
+		count = count + 1;
+	}
+
+
+	return count;
 }
 
 // val_array の配列に格納されたarray_sizeの数字の任意の組み合わせで値の和がsumになる組み合わせは何個ありますか？
 int calc_combinations(int sum, const int* val_array, int array_size)
 {
-	return 0;
+	float count = 0;
+	int i;
+	float big = 1;
+	int min = 1;
+	int b = 0;
+	float a = 0;
+	int purasu = 0;
+	//1と2
+
+	for (i = 0; i <= array_size; i++)
+	{
+		purasu = purasu + val_array[i];
+		if (val_array[i] == sum)
+		{
+			count = count + 1;
+		}
+		else if (purasu == sum)
+		{
+			count = count + 1;
+		}
+
+	}
+	purasu = 0;
+
+	if (sum != array_size)
+	{
+		for (i = 0; i < sum; i++)
+		{
+			purasu = purasu + val_array[i];
+		}
+		if (purasu == sum)
+		{
+			for (i = 0; i < sum; i++)
+			{
+				a = array_size - i;
+				big = big * a;
+				b = 1 + i;
+				min = b * min;
+			}
+			count = big / min;
+		}
+	}
+	return count;
 }
