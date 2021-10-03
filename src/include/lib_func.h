f7c28e2789f846b0777a72461b98f39f66c5416e
yuto-0122 <ireina0122@icloud.com>
2021-10-03 22:48:51 +0900

    関数の中身を実装しました。

--- src/include/lib_func.h
+++ src/include/lib_func.h
@@ -15,6 +15,7 @@ int calc_combinations_3(int sum, int val1, int val2, int val3);
 // val1 から val4 の任意の組み合わせで値の和がsumになる組み合わせは何個ありますか？
 int calc_combinations_4(int sum, int val1, int val2, int val3, int val4);
 
+
 // val_array の配列に格納されたarray_sizeの数字の任意の組み合わせで値の和がsumになる組み合わせは何個ありますか？
 int calc_combinations(int sum, const int* val_array, int array_size);
 
