27fb3e8979273f60e8866b242fe9bbf6fd757718
KatsumataShota <d35c77afd7t3yoe05se@softbank.ne.jp>
2021-10-05 18:42:55 +0900

    解法がわからなかったので遅れてしまいました

--- src/StaticLib/StaticLib.c
+++ src/StaticLib/StaticLib.c
@@ -30,11 +30,48 @@ int calc_combinations_3(int sum, int val1, int val2, int val3)
 // val1 から val4 の任意の組み合わせで値の和がsumになる組み合わせは何個ありますか？
 int calc_combinations_4(int sum, int val1, int val2, int val3, int val4)
 {
-	return 0;
+	int n = 0;
+
+	for (int n1 = 0; n1 < 2;n1++) {
+		for (int n2 = 0; n2 < 2; n2++) {
+			for (int n3 = 0; n3 < 2; n3++) {
+				for (int n4 = 0; n4 < 2; n4++) {
+					int s = 0;
+					if (n1 == 1)s += val1;
+					if (n2 == 1)s += val2;
+					if (n3 == 1)s += val3;
+					if (n4 == 1)s += val4;
+
+					if (s == sum)n++;
+				}
+			}
+		}
+	}
+	return n;
+	
 }
 
 // val_array の配列に格納されたarray_sizeの数字の任意の組み合わせで値の和がsumになる組み合わせは何個ありますか？
+int calc_combinations_sub(int array_index,int num,int now,int sum, const int* val_array, int array_size) 
+{
+	if (now == sum) num++;
+
+	if (sum <= now)return num;
+
+	if (array_index < array_size) {
+		int val = val_array[array_index];
+
+		num=calc_combinations_sub(array_index + 1, num, now, sum, val_array, array_size);
+		
+		num=calc_combinations_sub(array_index + 1, num, now+val, sum, val_array, array_size);
+	}
+	return num;
+}
+
+
+
+
 int calc_combinations(int sum, const int* val_array, int array_size)
 {
-	return 0;
+	return calc_combinations_sub(0, 0, 0, sum, val_array, array_size);
 }
